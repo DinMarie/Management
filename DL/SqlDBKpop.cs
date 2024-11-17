b23c5eb6ae11ec9d43546b287a6decd35d61c541
karyle anne <karyle anne@LAPTOP-GSQAHJBR>
2024-11-16 20:18:51 -0900

    Edited

--- DL/SqlDBKpop.cs
+++ DL/SqlDBKpop.cs
@@ -13,8 +13,8 @@ namespace DL
     {
         static string connectionString
         //  = "Server = tcp:20.2.249.96,1433; Database = KPOP_DB; User Id = sa; Password = integ2!";
-        //= "Data Source =LAPTOP-GSQAHJBR; Initial Catalog = KPOP_DB; Integrated Security = True;";
-        = "Data Source =LAPTOP-LGBEJ5GN\\SQLEXPRESS02; Initial Catalog = KPOP_DB; Integrated Security = True;";
+        = "Data Source =LAPTOP-GSQAHJBR; Initial Catalog = KPOP_DB; Integrated Security = True;";
+        //= "Data Source =LAPTOP-LGBEJ5GN\\SQLEXPRESS02; Initial Catalog = KPOP_DB; Integrated Security = True;";
 
 
 
--- ManagementAPI/Controllers/ManagementController.cs
+++ ManagementAPI/Controllers/ManagementController.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using KpopBL;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ManagementAPI.Controllers
 {
@@ -10,10 +15,16 @@ namespace ManagementAPI.Controllers
         UserGetServices getServices;
         Business transactionService;
 
+        private readonly string _accessKey = "";
+        private readonly string _secretKey = "";
+        private readonly string _bucketName = "jmnaval";
+        private readonly AmazonS3Client _s3Client;
+
         public ManagementController()
         {
             getServices = new UserGetServices();
             transactionService = new Business();
+            _s3Client = new AmazonS3Client(_accessKey, _secretKey, Amazon.RegionEndpoint.USEast1);
         }
 
         [HttpGet]
@@ -29,13 +40,13 @@ namespace ManagementAPI.Controllers
             }
             return cont;
         }
+
         [HttpPost("Add")]
         public JsonResult AddGroup(Group request)
         {
             var result = transactionService.CreateGroup(request.GroupID, request.Name);
 
             return new JsonResult(result);
-
         }
 
         [HttpPatch("Update")]
@@ -49,16 +60,52 @@ namespace ManagementAPI.Controllers
         [HttpDelete]
         public JsonResult DeleteRoom(ManagementAPI.Group request)
         {
-
             var groupToDelete = new Modelsa.Group
             {
                 GroupID = request.GroupID
-
             };
 
             var result = transactionService.DeleteGroup(groupToDelete);
 
             return new JsonResult(result);
+        }
+
+        // New endpoint to upload a file to S3
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFileToS3(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file provided.");
+            }
+
+            var fileTransferUtility = new TransferUtility(_s3Client);
+
+            try
+            {
+                using (var fileStream = file.OpenReadStream())
+                {
+                    var uploadRequest = new TransferUtilityUploadRequest
+                    {
+                        InputStream = fileStream,
+                        Key = file.FileName, // S3 object key (name of the file)
+                        BucketName = _bucketName,
+                        ContentType = file.ContentType
+                    };
+
+                    await fileTransferUtility.UploadAsync(uploadRequest);
+                }
+
+                return Ok("File uploaded successfully.");
+            }
+            catch (AmazonS3Exception e)
+            {
+                return StatusCode(500, $"Error encountered: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, $"Unexpected error: {e.Message}");
+            }
         }
     }
 }
